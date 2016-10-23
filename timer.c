bc7058a641ea9f554262e5dd1d1a380d6d87111c
NastyaGurskaya <just.gurskaya@gmail.com>
2016-10-24 01:15:20 +0300

    Implement init and exit functions

--- timer.c
+++ timer.c
@@ -14,11 +14,23 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Nastya Gurskaya");
 MODULE_VERSION("1");
 
+#define DELAY 3
+#define TEXT "Hello, world!"
 
-static void print_hello(unsigned long data)
-{
-		printk(KERN_INFO "Hello, world!\n");	
-}
+static unsigned long times_left = 0;
+static unsigned char timer_exists = 0;
+
+static int timer_init(void);
+static void timer_exit(void);
+
+static ssize_t times_write(struct kobject *, struct kobj_attribute *, char *);
+static ssize_t times_read(struct kobject *, struct kobj_attribute *, const char *, size_t);
+
+static struct kobj_attribute times_attrb =
+    __ATTR(interval, 0664, timer_write, timer_read);
+
+static struct kobject *times_obj = NULL;
+static struct timer_list timer;
 
 static ssize_t timer_read(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
 {
@@ -33,14 +45,29 @@ static ssize_t timer_write(struct kobject *kobj, struct kobj_attribute *attr, co
 
 static int timer_init(void)
 {
-	
+	nit_timer_on_stack(&timer);
+
+    times_obj = kobject_create_and_add("timer", NULL);
+    if (!times_obj) {
+        return -ENOMEM;
+    }
+
+    if (sysfs_create_file(times_obj, &times_attrb.attr)) {
+        timer_exit();
+        return -EINVAL;
+    }
 	return 0;
 }
 
 static void timer_exit(void)
 {	
-	kobject_put(mytimer_kobject);
-	del_timer(&my_timer);
+     if (timer_exists) {
+        del_timer(&timer);
+    }
+
+    if (times_obj) {
+        kobject_put(times_obj);
+    }
 }
 
 module_init(timer_init);
